15e023ca35ec88329ec3618781971eba394e8ecd
Emma Bishop <emma.b.bishop@gmail.com>
2016-04-19 12:26:38 -0400

    Renames starting point

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ var app = express();
 
 server.on('request', app);
 
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(path.join(__dirname, 'browser')));
-
+app.use(logger('dev'));
 app.get('/', function(req, res, next){
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(3000, function () {
-  console.log('listening on port 3000');
-})
+server.listen(app.get('port'), function () {
+  console.log('listening on port', app.get('port'));
+});
